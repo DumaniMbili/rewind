5da4bad162e83f9565e749790cffff57c292320a
Dumani Mbili <dumanimbili@gmail.com>
2023-06-20 21:16:59 +0200

    included whatsapp bot

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ hamburger.onclick = function () {
 };
 
 
+// Replace "YOUR_PHONE_NUMBER" with your actual phone number, including the country code
+const phoneNumber = "076 556 4827";
+
+// Replace "YOUR_DEFAULT_MESSAGE" with the default message you want to send
+const defaultMessage = "Please feel free to give us a chat if you want to know anything 'Rewind'. ";
+
+const whatsappBot = document.querySelector(".whatsapp-bot");
+
+whatsappBot.addEventListener("click", () => {
+    // Open WhatsApp chat when the bot is clicked
+    window.open(`https://wa.me/${phoneNumber}?text=${encodeURIComponent(defaultMessage)}`);
+});
